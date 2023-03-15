20924bece9b17bfeaa935e28f5725f7d3ea26110
Borislav Popov <BorislavP@grafixoft.com>
2023-03-15 19:56:15 +0200

    Improve game list styling

--- src/app/Interfaces/game.interface.ts
+++ src/app/Interfaces/game.interface.ts
@@ -1,6 +1,8 @@
 import { GameStatusEnum } from '../enums/game-status.enum';
+import { IPlayer } from './player.interface';
 
 export interface IGame {
     id: number;
     status: GameStatusEnum;
+    players: IPlayer[];
 }
